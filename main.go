46ac0c02613a839f44dac2b90a004d17437ba04a
Joshua Daniels <joshuadaniels537@gmail.com>
2023-06-21 22:08:38 +0200

    improvement: neatly separate lines

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -28,11 +29,27 @@ func fileExists() {
 
 func fileDoesNotExist(fileName string) {
 
-	err := ioutil.WriteFile(fileName, []byte("Journal Entry\n\n"), 0644)
+	fileBoilerplate := getFileBoilerplate()
+
+	err := ioutil.WriteFile(fileName, []byte(fileBoilerplate), 0644)
 	if err != nil {
 		fmt.Printf("%s '%s'\n",errorPrefix, err)
 		return
 	}
 
 	fmt.Printf("Your file '%s' has been created. Ya welcome!\n", fileName)
+}
+
+
+func getFileBoilerplate() string {
+	lineSeparator := "\n------------------------------\n\n\n"
+	heading := "Journal Entry"
+	howDidYouFeel := "How did you feel today?"
+	whatDidYouDo := "What did you do today?"
+	interestingThoughts := "Any interesting thoughts?"
+
+	fileLines := []string{heading, howDidYouFeel, whatDidYouDo, interestingThoughts}
+
+
+	return strings.Join(fileLines, lineSeparator) + lineSeparator
 }
